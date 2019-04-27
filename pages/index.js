7b5c3b37284b4eab6ed40851709d17cc3140a47b
Jake Wiesler <jakewiesler@gmail.com>
2019-04-27 14:59:50 -0400

    refactor: use layout in pages/index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Head from 'next/head';
+import { Layout } from '../components';
 
-function Home() {
-  return <div>Welcome to Next.js!</div>;
+function Inventory() {
+  return (
+    <>
+      <Head>
+        <title>Inventory | Watto's Space Emporium</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Layout>Inventory page</Layout>
+    </>
+  );
 }
 
-export default Home;
+export default Inventory;
