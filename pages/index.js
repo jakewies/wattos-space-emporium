057ce5daecbf8eb8bafc48438afad11099416d89
Jake Wiesler <jakewiesler@gmail.com>
2019-04-28 13:29:30 -0400

    refactor: update inventory page
    Added a PageHeader and a Toolbar with 2 Select inputs.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
-import { Layout, PageHeader } from '../components';
+import styled from 'styled-components';
+import { Layout, PageHeader, Select } from '../components';
 
 function Inventory() {
   return (
@@ -11,9 +12,63 @@ function Inventory() {
       </Head>
       <Layout>
         <PageHeader text="Inventory" />
+        <Toolbar>
+          <Select
+            label="Class"
+            options={['Starfighter', 'Light freighter', 'Transport shuttle']}
+            onSelect={value => console.log(value)}
+            css={`
+              margin-right: 1rem;
+            `}
+          />
+          <Select
+            label="Manufacturer"
+            options={[
+              'Sienar Fleet Systems',
+              'Incom Corporation',
+              'Koensayr Manufacturing',
+              'Corellian Engineering Corporation',
+              'Cygnus Spaceworks',
+              'Alliance Underground Engineering',
+              'Gial Ackbar'
+            ]}
+            onSelect={value => console.log(value)}
+          />
+          <ResultCount>
+            <span>Showing 8 results</span>
+          </ResultCount>
+        </Toolbar>
+        <InventoryList>Im inventory list</InventoryList>
       </Layout>
     </>
   );
 }
 
 export default Inventory;
+
+const Toolbar = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.25rem;
+`;
+
+const ResultCount = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  height: ${({ theme }) => theme.pillHeight};
+
+  > span {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    padding: 1rem;
+    background-color: ${({ theme }) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.white};
+    border-radius: 100px;
+    font-weight: 600;
+    font-size: 14px;
+  }
+`;
+
+const InventoryList = styled.div``;
