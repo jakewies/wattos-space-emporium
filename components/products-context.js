b07b6cb777261ba2875f68146c3c67f591da9da0
Jake Wiesler <jakewiesler@gmail.com>
2019-04-29 12:11:00 -0400

    feat: setup serverless deployment

--- components/products-context.js
+++ components/products-context.js
@@ -1,36 +1,43 @@
 import React, { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
 import FullScreenLoading from './fullscreen-loading';
-import firebase from '../lib/firebase';
+
+const FIREBASE_DATABASE_URL =
+  'https://wattos-spaceship-emporium.firebaseio.com';
 
 export const ProductsContext = createContext([]);
 export const ProductsConsumer = ProductsContext.Consumer;
 
 export function ProductsProvider({ children }) {
   const [products, setProducts] = useState([]);
+  const [fetching, setFetching] = useState(true);
+
+  const getProducts = async () => {
+    try {
+      const { data } = await axios.get(
+        `${FIREBASE_DATABASE_URL}/products.json`
+      );
+      setProducts(
+        Object.keys(data).map(productId => ({
+          ...data[productId],
+          id: parseInt(productId)
+        }))
+      );
+    } catch (err) {
+      console.error(err);
+      setProducts([]);
+    }
+    setFetching(false);
+  };
 
   const getProductById = id =>
     products.find(product => parseInt(product.id) === parseInt(id));
 
   useEffect(() => {
-    const productsRef = firebase.database().ref('products');
-
-    productsRef.on('value', snapshot => {
-      const productsMap = snapshot.val();
-
-      setProducts(
-        Object.keys(productsMap).map(productId => ({
-          ...productsMap[productId],
-          id: parseInt(productId)
-        }))
-      );
-    });
-
-    return () => {
-      productsRef.off();
-    };
+    getProducts();
   }, []);
 
-  if (!products.length) {
+  if (fetching) {
     return <FullScreenLoading text="Loading Emporium Archive..." />;
   }
 
--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-const { parsed: localEnv } = require('dotenv').config();
-const webpack = require('webpack');
-
 module.exports = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-
-    return config;
-  }
+  target: 'serverless'
 };
